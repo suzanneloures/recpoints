b7a08af9d115233a1c83ba18e5f1705a683712a9
suzanneloures <suzanneloures>
2019-04-26 00:29:41 -0300

    funcao predict

--- recommender.py
+++ recommender.py
@@ -1,24 +1,23 @@
 import pandas as pd
 from surprise import Reader, Dataset, SVD, evaluate
+import json
 
-review = ({'user_id': ['0', '0', '0', '1', '1', '2', '2', '2'],
-                     'item_id': ['a', 'b', 'c', 'a', 'b', 'b', 'c', 'd'],
-                     'rating': [1, 3, 2, 5, 4, 1, 4, 3]})
-df = pd.DataFrame(review)
-#rating_scale param is requiered.
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[['user_id', 'item_id', 'rating']], reader)
-print (df)
-# treina com toda  a base
-trainset = data.build_full_trainset()
 
-# define o algoritmo 
-algo = SVD()
+def load_json():
+    with open('data/review.json') as json_file:  
+        dataset = json.load(json_file)
+        return dataset
 
-result = evaluate(algo, data, measures=['RMSE'])
+def train(uid, iid):
+    dataset = load_json()   
+    df = pd.DataFrame.from_dict(dataset, orient='columns') #json para df
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(df[['user_id', 'item_id', 'rating']], reader)
+    trainset = data.build_full_trainset()
+    svd = SVD()
+    svd.fit(trainset)
+    pred = svd.predict(uid, iid)
+    score = pred.est
+    print(score)
 
-uid = str(0)  
-iid = str('b') 
-
-# get a prediction for specific users and items.
-pred = algo.predict(uid, iid, r_ui=1, verbose=True)
+train(999,'59392558')
